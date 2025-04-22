f70795f2728ad84a6866fa7fefd0e3ad89db956b
Jack Hawkins <jack.m.hawkins@vanderbilt.edu>
2025-04-22 10:10:02 -0500

    API endpoint to filter for unassigned dogs based on their city

--- Models/DTOs/DogDTO.cs
+++ Models/DTOs/DogDTO.cs
@@ -4,6 +4,7 @@ public class DogDTO
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public int CityId { get; set; }
     public int? WalkerId { get; set; }
     public string? WalkerName { get; set; }
 }
